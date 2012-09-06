d5cebb02f426e481c979900eb627373ab8dd13e5
jaischeema <jaischeema@gmail.com>
2012-09-06 21:41:21 +1000

    Add the unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,8 @@
-# http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
+root = "/var/www/live/errbit/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
