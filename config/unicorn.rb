937fb1f5f6dfb8f5cedd18c0b8a6200f6d1d79bb
jaischeema <jaischeema@gmail.com>
2012-09-12 09:24:24 +1000

    Fix unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,5 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
+listen "/tmp/unicorn.errbit.sock"
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
